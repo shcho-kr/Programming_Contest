f86587897996b004b92684d0d970b2658a375d98
shcho.pc <kyushu1996@gmail.com>
2022-10-05 00:31:59 +0900

    Ready For Coding

---
+++ base.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	
+    cout<<"Hello world!"<<endl;
+
+	return 0;
+}
