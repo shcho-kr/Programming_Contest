509bd6c3568c394d2b97ca3d30fcd208e1381680
shcho.pc <kyushu1996@gmail.com>
2022-10-27 02:40:06 +0900

    yukicode zz

--- YukiCoder/2099.cpp
+++ YukiCoder/2099.cpp
@@ -8,27 +8,38 @@
 #include <numeric>
 #include <cmath>
 #define ll long long
+#define endl '\n'
 using namespace std;
-
-// link : 
 // category : 
 
 int main() {
-	
-    int t, x, a, y, b, ans=0;
-    cin>>t>>x>>a>>y>>b;
+    ios_base::sync_with_stdio(false);
+    
+    ll t,x,a,y,b; cin>>t>>x>>a>>y>>b;
+    ll n,m,l,ans=20000000ll*20000000ll;
+
+    // min of x*n+y*m+l
+    // when a*n-b*m+l = t
 
     if(t>0){
-        int day=0, spend=0;
-        while(day<t){
-            day+=a;
-            spend+=x;
+        // m=0 ~ (t-floor(t/a)*a) + floor(t/a)*x
+        m=(t-floor(t/a)*a) + floor(t/a)*x;
+        for(int i=0;i<=m;i++){
+            n=floor((t+b*i)/a);
+            l=t-a*n+b*i;
+            ans=min(n*x+i*y+l,ans);
         }
     }
-    else {
-        
-    }
+    else{
+        // m=ceil(|t|/b) ~ ceil(|t|/b)+ floor((ceil(|t|/b)*b-|t|)/y)
+        m=ceil(double(abs(t))/b)+ floor((ceil(double(abs(t))/b)*b-abs(t))/y);
+        for(int i=ceil(double(abs(t))/b);i<=m;i++){
+            n=floor((b*i-abs(t))/a);
+            l=b*i-abs(t)-a*n;
+            ans=min(n*x+i*y+l,ans);
+        }
 
+    }
     cout<<ans<<endl;
 
 	return 0;
--- YukiCoder/2100.cpp
+++ YukiCoder/2100.cpp
@@ -8,14 +8,70 @@
 #include <numeric>
 #include <cmath>
 #define ll long long
+#define endl '\n'
 using namespace std;
-
-// link : 
 // category : 
 
 int main() {
-	
+    ios_base::sync_with_stdio(false);
     
+    int n,m; cin>>n>>m;
+
+	vector<vector<ll>> dp1(2,vector<ll>(n+1,0));
+	vector<vector<ll>> dp2(2,vector<ll>(n+1,0));
+
+	vector<vector<bool>> flag1(2,vector<bool>(n+1,0));
+	vector<vector<bool>> flag2(2,vector<bool>(n+1,0));
+
+	flag1[0][1]=true;
+	flag2[0][n]=true;
+	
+	int h[n+1]={}; for(int i=1;i<=n;i++) cin>>h[i];
+
+	vector<set<int>> v(n+1);
+	for(int i=0;i<m;i++){
+		int x,y; cin>>x>>y;
+		v[x].insert(y);
+		v[y].insert(x);
+	}
+
+	for(int i=2;i<=n;i++){
+		for(auto& j : v[i]){
+			if(!(flag1[0][j]|flag1[1][j])) continue;
+			
+			if(h[i]>h[j]){
+				if(flag1[0][j]){
+					dp1[1][i]=max(dp1[1][i],dp1[0][j]+h[i]-h[j]);
+					flag1[1][i]=true;
+				}
+			}
+			else{
+				dp1[0][i]=max({dp1[0][i],dp1[0][j],dp1[1][j]});
+				flag1[0][i]=true;
+			}
+		}
+	}
+	if(flag1[0][n]|flag1[1][n])cout<<max(dp1[0][n],dp1[1][n])<<endl;
+	else cout<<-1<<endl;
+
+	for(int i=n-1;i>=1;i--){
+		for(auto& j : v[i]){
+			if(!(flag2[0][j]|flag2[1][j])) continue;
+			
+			if(h[i]>h[j]){
+				if(flag2[0][j]){
+					dp2[1][i]=max(dp2[1][i],dp2[0][j]+h[i]-h[j]);
+					flag2[1][i]=true;
+				}
+			}
+			else{
+				dp2[0][i]=max({dp2[0][i],dp2[0][j],dp2[1][j]});
+				flag2[0][i]=true;
+			}
+		}
+	}
+	if(flag2[0][1]|flag2[1][1]) cout<<max(dp2[0][1],dp2[1][1])<<endl;
+	else cout<<-1<<endl;
 
 	return 0;
 }
